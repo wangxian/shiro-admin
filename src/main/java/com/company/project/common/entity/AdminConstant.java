5ac47635af32cefdc3b3e30936355a0061d5d860
wangxian <xian366@126.com>
2019-07-12 21:11:53 +0800

    修改：admin_captcha_

--- src/main/java/com/company/project/common/entity/AdminConstant.java
+++ src/main/java/com/company/project/common/entity/AdminConstant.java
@@ -16,7 +16,7 @@ public class AdminConstant {
     public static final String VIEW_PREFIX = "admin/views/";
 
     // 验证码 Session Key
-    public static final String CODE_PREFIX = "febs_captcha_";
+    public static final String CODE_PREFIX = "admin_captcha_";
 
     // 允许下载的文件类型，根据需求自己添加（小写）
     public static final String[] VALID_FILE_TYPE = {"xlsx", "zip"};
