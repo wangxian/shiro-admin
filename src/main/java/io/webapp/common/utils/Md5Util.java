1c9566d07f682fed757c8b45a1ec97dd7261aafc
wangxian <xian366@126.com>
2021-02-16 00:45:50 +0800

    更新：同步官方，fix typo Md5Util

--- src/main/java/io/webapp/common/utils/Md5Util.java
+++ src/main/java/io/webapp/common/utils/Md5Util.java
@@ -34,7 +34,7 @@ public class Md5Util {
         }
     }
 
-    private static final String ALGORITH_NAME = "md5";
+    private static final String ALGORITHM_NAME = "md5";
 
     private static final int HASH_ITERATIONS = 5;
 
@@ -48,6 +48,6 @@ public class Md5Util {
     public static String encrypt(String username, String password) {
         String source = StringUtils.lowerCase(username);
         password = StringUtils.lowerCase(password);
-        return new SimpleHash(ALGORITH_NAME, password, ByteSource.Util.bytes(source), HASH_ITERATIONS).toHex();
+        return new SimpleHash(ALGORITHM_NAME, password, ByteSource.Util.bytes(source), HASH_ITERATIONS).toHex();
     }
 }
