e3f06af6e707d1d1a3f2948bfc791ca67cf0ae43
wangxian <xian366@126.com>
2021-02-17 00:28:01 +0800

    更新：同步官方，启动去除redis检查

--- src/main/java/io/webapp/common/runner/AdminStartedUpRunner.java
+++ src/main/java/io/webapp/common/runner/AdminStartedUpRunner.java
@@ -1,7 +1,6 @@
 package io.webapp.common.runner;
 
 import io.webapp.common.properties.AdminProperties;
-import io.webapp.common.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -23,7 +22,6 @@ public class AdminStartedUpRunner implements ApplicationRunner {
 
     private final ConfigurableApplicationContext context;
     private final AdminProperties adminProperties;
-    private final RedisService redisService;
 
     @Value("${server.port:8080}")
     private String port;
@@ -33,21 +31,6 @@ public class AdminStartedUpRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        try {
-            // 测试 Redis连接是否正常
-            redisService.hasKey("admin_test");
-        } catch (Exception e) {
-            log.error(" ____   __    _   _ ");
-            log.error("| |_   / /\\  | | | |");
-            log.error("|_|   /_/--\\ |_| |_|__");
-            log.error("                        ");
-            log.error("ADMIN 启动失败，{}", e.getMessage());
-            log.error("Redis连接异常，请检查Redis连接配置并确保Redis服务已启动");
-
-            // 关闭 ADMIN
-            context.close();
-        }
-
         if (context.isActive()) {
             InetAddress address = InetAddress.getLocalHost();
             String url = String.format("http://%s:%s", address.getHostAddress(), port);
