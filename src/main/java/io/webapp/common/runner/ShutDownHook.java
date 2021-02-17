d4019c85bdeee8559bb5f2e63d49801302fdd310
wangxian <xian366@126.com>
2021-02-17 17:13:41 +0800

    更新：同步官方，ShutDownHook

--- src/main/java/io/webapp/common/runner/ShutDownHook.java
+++ src/main/java/io/webapp/common/runner/ShutDownHook.java
@@ -1,15 +1,17 @@
 package io.webapp.common.runner;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.DisposableBean;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class ShutDownHook implements DisposableBean {
-
-    @Override
-    public void destroy() {
+public class ShutDownHook {
+    @EventListener(classes = {ContextClosedEvent.class})
+    public void onApplicationClosed(@NonNull ApplicationEvent event) {
         log.info("ADMIN管理系统已关闭，Bye！");
     }
 }
